d71e77bb6d5d126036f45aa63129fe2153342fae
kyleschaeffer <kyle@spirito.us>
2019-05-31 10:47:25 -0400

    Add Chart.js, build sample chart data

--- src/webparts/salesChart/components/SalesChart.tsx
+++ src/webparts/salesChart/components/SalesChart.tsx
@@ -1,4 +1,4 @@
-import { escape } from '@microsoft/sp-lodash-subset';
+import { Bar, HorizontalBar, Line, ChartData } from 'react-chartjs-2';
 import * as React from 'react';
 import styles from './SalesChart.module.scss';
 
@@ -14,7 +14,41 @@ export default class SalesChart extends React.Component<ISalesChartProps, {}> {
     return (
       <div>
         {this.props.chartTitle && <h1 className={styles.title}>{this.props.chartTitle}</h1>}
+
+        {this.props.chartType === 'barv' && <Bar data={this.data()} />}
+        {this.props.chartType === 'barh' && <HorizontalBar data={this.data()} />}
+        {this.props.chartType === 'line' && <Line data={this.data()} />}
       </div>
     );
+  }
+
+  // Generate chart data
+  private data(): ChartData<Chart.ChartData> {
+    // Chart data
+    const data: ChartData<Chart.ChartData> = {
+      labels: [
+        'Jan',
+        'Feb',
+        'Mar',
+        'Apr',
+        'May',
+      ],
+      datasets: [
+        {
+          label: 'Sample Dataset 1',
+          data: [10, 15, 5, 25, 20],
+          backgroundColor: `rgba(65, 123, 170, ${this.props.chartType === 'line' ? '0' : '1'})`,
+          borderColor: `rgba(65, 123, 170, 1)`,
+        },
+        {
+          label: 'Sample Dataset 2',
+          data: [25, 5, 0, 15, 30],
+          backgroundColor: `rgba(209, 70, 93, ${this.props.chartType === 'line' ? '0' : '1'})`,
+          borderColor: `rgba(209, 70, 93, 1)`,
+        },
+      ],
+    };
+
+    return data;
   }
 }
