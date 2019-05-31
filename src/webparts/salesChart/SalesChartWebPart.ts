6efdc68cb4b57288d26a741cebc29211d2f210c6
kyleschaeffer <kyle@spirito.us>
2019-05-31 10:09:35 -0400

    Install SPFx

---
+++ src/webparts/salesChart/SalesChartWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+
+import * as strings from 'SalesChartWebPartStrings';
+import SalesChart from './components/SalesChart';
+import { ISalesChartProps } from './components/ISalesChartProps';
+
+export interface ISalesChartWebPartProps {
+  description: string;
+}
+
+export default class SalesChartWebPart extends BaseClientSideWebPart<ISalesChartWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<ISalesChartProps > = React.createElement(
+      SalesChart,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/salesChart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISalesChartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SalesChartWebPartStrings' {
+  const strings: ISalesChartWebPartStrings;
+  export = strings;
+}
